c64ec6bd1c93ed4a14a1a41833f409c671edd7f1
Floride125-art <FTuyisenge120@daviscollege.com>
2021-05-04 21:49:17 +0200

    added home and profile pages

--- hood/views.py
+++ hood/views.py
@@ -3,3 +3,20 @@ from django.contrib.auth.decorators import login_required
 from . models import *
 from .forms import *
 from django.views import generic
+@login_required(login_url='/accounts/login/')
+def home(request):
+    neighbourhoods = Neighbourhood.objects.all()
+    return render(request, 'home.html',{"neighbourhoods":neighbourhoods,})
+def add_profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(id = current_user.id)
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            caption = form.save(commit=False)
+            caption.user = current_user
+            caption.save()
+            return redirect('myprofile')
+    else:
+        form = NewProfileForm()
+    return render(request, 'edit_profile.html', {"form":form})    
